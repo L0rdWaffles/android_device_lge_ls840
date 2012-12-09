b2d9b49ac259a8890ab840facccb203b7c6602b5
Ryan Norris <rnorris97@bertie.k12.nc.us>
2012-12-09 15:43:11 -0500

    Update build_fingerprint to latest android release.

--- cm.mk
+++ cm.mk
@@ -11,23 +11,23 @@ DATE := $(shell date +%Y%m%d)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lge/ls840/device_ls840.mk)
+$(call inherit-product, device/lge/ms840/device_ls840.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ls840
 PRODUCT_NAME := cm_ls840
 PRODUCT_BRAND := lge
-PRODUCT_MODEL := LG-ls840
+PRODUCT_MODEL := LG-LS840
 PRODUCT_MANUFACTURER := LGE
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_DEVICE=cayman \
-PRODUCT_NAME=cayman_mpcs_us \
-BUILD_ID=LG-MS910-CM9 \
-BUILD_DISPLAY_ID=LG-ls840-CM9 \
-BUILD_FINGERPRINT="lge/cayman_sprint_us/cayman:4.0.4/IMM76D/ls840ZV8.47A73A3A:user/release-keys" \
-PRIVATE_BUILD_DESC="cayman_sprint_us-user 4.0.4 IMM76D ls840ZV8 release-keys" \
+PRODUCT_NAME=cayman_sprint_us \
+BUILD_ID=LG-LS840-CM9 \
+BUILD_DISPLAY_ID=LG-LS840-CM9 \
+BUILD_FINGERPRINT="sprint_lge/cayman_sprint_us/cayman:4.0.4/ZVI.IMM76D/LS840ZVI.4820d7b4:user/release-keys" \
+PRIVATE_BUILD_DESC="cayman_sprint_us-user 4.0.4 IMM76D LS840ZVI release-keys" \
 TARGET_BUILD_TYPE=eng \
 BUILD_NUMBER=${DATE} 
