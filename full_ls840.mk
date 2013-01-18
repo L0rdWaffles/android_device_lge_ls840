456e065568c1d38611abeebc79dc5852bc35b5e1
Ryan Norris <rnorris97@bertie.k12.nc.us>
2013-01-18 16:25:27 -0500

    Ugh, why me?

--- full_ls840.mk
+++ full_ls840.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2009 The Android Open Source Project
+# Copyright (C) 2013 Ryan Norris
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,46 +19,6 @@
 # product configuration (apps).
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, device/samsung/d710/d710.mk)
-
-
-# Discard inherited values and use our own instead.
-PRODUCT_NAME := full_d710
-PRODUCT_DEVICE := d710
-PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := SPH-D710
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SPH-D710 TARGET_DEVICE=SPH-D710 BUILD_FINGERPRINT=samsung/SPH-D710/SPH-D710:4.0.4/IMM76I/FF18:user/release-keys  PRIVATE_BUILD_DESC="SPH-D710-user 4.0.4 IMM76I FF18 release-keys"
-
-=======
-
-# Copyright (C) 2011 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-#
-# This file is the build configuration for a full Android
-# build for maguro hardware. This cleanly combines a set of
-# device-specific aspects (drivers) with a device-agnostic
-# product configuration (apps). Except for a few implementation
-# details, it only fundamentally contains two inherit-product
-# lines, full and maguro, hence its name.
-#
-
 # Get the long list of APNs
 PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
@@ -67,14 +27,17 @@ PRODUCT_PACKAGES := \
     Camera
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-# This is where we'd set a backup provider if we had one
-#$(call inherit-product, device/sample/products/backup_overlay.mk)
-# Inherit from ls840 device
 $(call inherit-product, device/lge/ls840/device_ls840.mk)
 
-# Set those variables here to overwrite the inherited values.
+
+# Discard inherited values and use our own instead.
+
 PRODUCT_NAME := full_ls840
 PRODUCT_DEVICE := ls840
-PRODUCT_BRAND := Sprint
-PRODUCT_MODEL := LG-ls840
+PRODUCT_BRAND := LGE
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_MODEL := LS840
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LS840 TARGET_DEVICE=LS840 BUILD_FINGERPRINT=LGE/LS840/LS840:4.0.4/IMM76I/ZVI:user/release-keys  PRIVATE_BUILD_DESC="LS840-user 4.0.4 IMM76I ZVI release-keys"
