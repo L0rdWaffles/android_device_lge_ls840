8f800543bb0c0353129fbc0848dd1b1b4f08800a
Ryan Norris <rnorris97@bertie.k12.nc.us>
2013-01-18 17:25:43 -0500

    Fixed.

--- cm.mk
+++ cm.mk
@@ -1,12 +1,8 @@
-# Consider cleaning up this makefile.
-
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/cdma.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := CM10-LG-Viper
-UTC_DATE := $(shell date +%s)
-DATE := $(shell date +%Y%m%d)
+PRODUCT_RELEASE_NAME := LS840
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -18,17 +14,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ls840
 PRODUCT_NAME := cm_ls840
-PRODUCT_BRAND := lge
-PRODUCT_MODEL := LG-LS840
+PRODUCT_BRAND := LGE
 PRODUCT_MANUFACTURER := LGE
+PRODUCT_MODEL := LS840
 
-#Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += \
-PRODUCT_DEVICE=cayman \
-PRODUCT_NAME=cayman_sprint_us \
-BUILD_ID=LG-LS840-CM10 \
-BUILD_DISPLAY_ID=LG-LS840-CM10 \
-BUILD_FINGERPRINT="sprint_lge/cayman_sprint_us/cayman:4.0.4/ZVI.IMM76D/LS840ZVI.4820d7b4:user/release-keys" \
-PRIVATE_BUILD_DESC="cayman_sprint_us-user 4.0.4 IMM76D LS840ZVI release-keys" \
-TARGET_BUILD_TYPE=eng \
-BUILD_NUMBER=ZVK-10.0.0
+#Set build fingerprint / ID / Prduct Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LS840 TARGET_DEVICE=LS840 BUILD_FINGERPRINT="sprint_lge/cayman_sprint_us/cayman:4.0.4/ZVI.IMM76D/LS840ZVI.4820d7b4:user/release-keys"  PRIVATE_BUILD_DESC="cayman_sprint_us-user 4.0.4 IMM76D LS840ZVI release-keys"
