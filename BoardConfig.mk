79cbdcf006886e3f314b417f16df0e7aec2abdef
Ryan Norris <rnorris97@bertie.k12.nc.us>
2013-01-19 18:59:45 -0500

    Fix wireless lan paths.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,6 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 TARGET_NO_BOOTLOADER := true
 
 # Camera
-#COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_HARDWARE
 BOARD_USES_PROPRIETARY_LIBCAMERA := true
 
 # HWComposer
@@ -122,9 +121,9 @@ BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
 WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/dhd.ko"
 WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcm4330_sta.bin"
-WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcm4330_apsta.bin"
-WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/wifi/bcm4330_p2p.bin"
+WIFI_DRIVER_FW_PATH_STA          := "/system/etc/firmware/bcm4330_sta.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/system/etc/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/firmware/fw_bcmdhd_p2p.bin"
 WIFI_DRIVER_MODULE_NAME          := "dhd"
 WIFI_DRIVER_MODULE_ARG           := "firmware_path=/system/etc/wifi/bcm4330_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
 WIFI_BAND                        := 802_11_ABG
